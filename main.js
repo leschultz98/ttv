1338778c5cc1caf3e59375eed24c639a12b6118a
phuc.le <phuc.le@s3corp.com.vn>
2026-04-06 12:49:35 +0700

    feat: content

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
+import { readFileSync } from 'fs';
 import express from 'express';
 
 const HOST = 'https://truyen.tangthuvien.vn';
-const REGEX = /<div class="box-chap[^>]*>([\s\S]*?)<\/div>/;
+
+const TITLE_REGEX = /<title>([\s\S]*?)<\/title>/;
+const CONTENT_REGEX = /<div class="box-chap[^>]*>([\s\S]*?)<\/div>/;
+
+const HTML = readFileSync('index.html', 'utf8');
 
 const app = express();
 
-app.get('*splat', async (req, res) => {
-  const path = req.params.splat.join('/');
-  const targetUrl = `${HOST}${path}`;
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('*all', async (req, res) => {
+  const targetUrl = `${HOST}${req.path}`;
 
   try {
     const response = await fetch(targetUrl);
     const text = await response.text();
-    const match = text.match(REGEX);
-    res.send(match[1]);
+
+    const title = text.match(TITLE_REGEX)[1];
+    const content = text.match(CONTENT_REGEX)[1].trim();
+
+    res.send(HTML.replace('__TITLE__', title).replace('__CONTENT__', content));
   } catch (err) {
-    res.status(500);
+    res.sendStatus(500);
   }
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
