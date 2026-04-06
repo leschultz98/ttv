7c7c858f2dc6540bdfec1e10bd662f9618271221
phuc.le <phuc.le@s3corp.com.vn>
2026-04-06 14:15:48 +0700

    feat: handle redirect

--- main.js
+++ main.js
@@ -16,26 +16,33 @@ app.get('/', (req, res) => {
 });
 
 app.get('*all', async (req, res) => {
-  const number = +req.path.match(NUMBER_REGEX)[1];
+  const number = +req.path.match(NUMBER_REGEX)?.[1];
+
+  if (isNaN(number)) {
+    res.redirect('/');
+    return;
+  }
+
   const targetUrl = `${HOST}${req.path}`;
 
-  try {
-    const response = await fetch(targetUrl);
-    const text = await response.text();
+  const response = await fetch(targetUrl, { redirect: 'manual' });
 
-    const data = {
-      __TITLE__: text.match(TITLE_REGEX)[1],
-      __CONTENT__: text.match(CONTENT_REGEX)[1].trim(),
-      __PREVIOUS__: req.path.replace(NUMBER_REGEX, 'chuong-' + (number - 1)),
-      __NEXT__: req.path.replace(NUMBER_REGEX, 'chuong-' + (number + 1)),
-    };
-
-    const result = Object.entries(data).reduce((acc, [key, value]) => acc.replaceAll(key, value), HTML);
-
-    res.send(result);
-  } catch (err) {
-    res.sendStatus(500);
+  if (response.status === 302) {
+    res.redirect('/');
+    return;
   }
+
+  const text = await response.text();
+  const data = {
+    __TITLE__: text.match(TITLE_REGEX)[1],
+    __CONTENT__: text.match(CONTENT_REGEX)[1].trim(),
+    __PREVIOUS__: req.path.replace(NUMBER_REGEX, 'chuong-' + (number - 1)),
+    __NEXT__: req.path.replace(NUMBER_REGEX, 'chuong-' + (number + 1)),
+  };
+
+  const result = Object.entries(data).reduce((acc, [key, value]) => acc.replaceAll(key, value), HTML);
+
+  res.send(result);
 });
 
 app.listen(process.env.PORT || 3000);
