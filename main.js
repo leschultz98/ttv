d7c94fdcf45d44d6a1435165f007aba54d32dce7
phuc.le <phuc.le@s3corp.com.vn>
2026-04-06 11:44:06 +0700

    feat: init ttv

---
+++ main.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const HOST = 'https://truyen.tangthuvien.vn';
+const REGEX = /<div class="box-chap[^>]*>([\s\S]*?)<\/div>/;
+
+const app = express();
+
+app.get('*splat', async (req, res) => {
+  const path = req.params.splat.join('/');
+  const targetUrl = `${HOST}${path}`;
+
+  try {
+    const response = await fetch(targetUrl);
+    const text = await response.text();
+    const match = text.match(REGEX);
+    res.send(match[1]);
+  } catch (err) {
+    res.status(500);
+  }
+});
+
+app.listen(3000);
