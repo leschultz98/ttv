f1e0e1a27900347cd07037b1dd2803fc6ba45c48
phuc.le <phuc.le@s3corp.com.vn>
2026-04-09 11:22:01 +0700

    refactor: move parse logic to client

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
+import http from 'http';
 import { readFileSync } from 'fs';
-import express from 'express';
 
-// import parse, { HOST } from './sources/ttv.js';
-import parse, { HOST } from './sources/wattpad.js';
+const HOST = 'https://wattpad.com.vn';
 
-const TITLE_REGEX = /<title>([\s\S]*?)<\/title>/;
-const HTML = readFileSync('index.html', 'utf8');
+const server = http.createServer(async (req, res) => {
+  switch (true) {
+    case req.url.startsWith('/api'): {
+      const response = await fetch(HOST + req.url.slice(4));
+      const text = await response.text();
+      res.writeHead(200);
+      res.end(text);
+      break;
+    }
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.sendStatus(200);
+    default:
+      const template = req.url === '/' ? 'index.html' : 'webnovel.html';
+      const data = readFileSync('templates/' + template);
+      res.writeHead(200);
+      res.end(data);
+  }
 });
 
-app.get('*all', async (req, res) => {
-  const targetUrl = `${HOST}${req.path}`;
+const PORT = process.env.PORT || 3000;
 
-  const response = await fetch(targetUrl);
-  const text = await response.text();
-
-  const data = parse(text, req.path);
-  data.__TITLE__ = text.match(TITLE_REGEX)[1];
-
-  const result = Object.entries(data).reduce((acc, [key, value]) => acc.replaceAll(key, value), HTML);
-
-  res.send(result);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('http://localhost:' + port);
+server.listen(PORT, () => {
+  console.log('http://localhost:' + PORT);
 });
