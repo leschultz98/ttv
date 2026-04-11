44e49954014fd7600ed85553c24ffc6306e1fbd5
phuc.le <leschultz98@outlook.com.vn>
2026-04-11 15:57:25 +0700

    feat: last

--- main.js
+++ main.js
@@ -3,28 +3,35 @@ import { readFileSync } from 'fs';
 
 const HOST = 'https://wattpad.com.vn';
 
+let last, next;
+
 const server = http.createServer(async (req, res) => {
   switch (true) {
     case req.url === '/favicon.ico': {
-      res.writeHead(204, {
-        'Cache-Control': 'public, max-age=31536000, immutable',
-      });
+      res.writeHead(204, { 'Cache-Control': 'public, max-age=31536000, immutable' });
       return res.end();
     }
 
     case req.url.startsWith('/api'): {
-      const response = await fetch(HOST + req.url.slice(4));
+      last = next;
+      next = req.url.slice(4);
+      const response = await fetch(HOST + next);
       const text = await response.text();
       res.writeHead(200);
-      res.end(text);
-      break;
+      return res.end(text);
     }
 
-    default:
+    case req.url === '/last':
+      res.writeHead(302, { Location: last || '/' });
+      return res.end();
+
+    default: {
+      last = next = req.url;
       const template = req.url === '/' ? 'index.html' : 'webnovel.html';
       const data = readFileSync('templates/' + template);
       res.writeHead(200);
       res.end(data);
+    }
   }
 });
 
