e5517c2d6d3022571c4d71684246c5f5bed2df16
phuc.le <leschultz98@outlook.com.vn>
2026-04-26 15:28:38 +0700

    feat: cookie

--- main.js
+++ main.js
@@ -15,7 +15,12 @@ const server = http.createServer(async (req, res) => {
     case req.url.startsWith('/api'): {
       last = next;
       next = req.url.slice(4);
-      const response = await fetch(HOST + next);
+      const response = await fetch(HOST + next, {
+        headers: {
+          cookie:
+            '__RequestVerificationToken=LOmW0U1U7gMNevGealoHW1ayoI5Std0MqjgXGqMiEjUtiIozKHwHgbmv38Cz87mrKHPbQDkFmosWedHJEOhe5C505pl6i71Y5fPv39pz7ZY1; cf_clearance=dXDWTpscer3WG8FdCM5EQ_LHJ_8NL93sAj0zy.1WZ8M-1777188215-1.2.1.1-7x8SKH_cSaIklyC2vM7H6UUBaZYwjAVLfZuYrAJmfbpSe_fafgkz0gPYLuwn16UTnJLbCUKwfz1av5iujFvsoYz12gXK9qYMm_F.cbLlR2GWO8SBDwNyoqk58YwYyGH9p4.A_8OPOQu7.dTVg82I3upZqv0UDJxiJ29pTmv7gQtQas7bo2ONeFte_RVGrA4RqspdhAq_qbqeKXl6b7XI2sfKdS.bCKwVAWSgUDamL2lhsBnK6xEGUiBfVSQg5KHP1DfKM0555T8IsJEqMdhGbt9QIG.qRR2bSgDL1hTOV73Fi03_wxSgMC.b0_0atFNW6A957DgMnGfbgUr4DGZ9ZQ; _gc=4',
+        },
+      });
       const text = await response.text();
       res.writeHead(200);
       return res.end(text);
